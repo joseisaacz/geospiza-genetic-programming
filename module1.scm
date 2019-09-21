7c2a57bef20d8693059615161cdc765b5147ff56
joseisaacz <39786721+joseisaacz@users.noreply.github.com>
2019-09-21 17:20:24 -0600

    other new things remember to put domain and codomain on each function please

--- module1.scm
+++ module1.scm
@@ -1,11 +1,20 @@
 (define operadores '( + - * expt / log))
 
-(define (randOperadores)
-  (+ 1 (random 6)))
+(define randOperadores
+  (lambda (n)
+  (+ 1 (random 6))))
 
+;;Dominio: Listas de cualquier tamano y un numero N natural mayor que 0
+;;Codominio: Elemento de la Posicion N 
 (define devuelvePos
   (lambda (L n)
     (cond ((null? L ) '())
           ((equal? 1 n) (car L))
           ((devuelvePos (cdr L) (- n 1) )))))
 
+;;Dominio: arboles binarios de expresion y numeros x y
+;;Codominio: resultado de la expresion evaluada con x y
+
+(define evalua
+  (lambda (L a b)
+    (eval (list (cons 'lambda (cons '(x y) (list L))) a b))))
