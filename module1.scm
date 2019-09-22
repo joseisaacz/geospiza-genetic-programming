45055d785869dd9cfd72bcd000a06688b1b4938e
joseisaacz <39786721+joseisaacz@users.noreply.github.com>
2019-09-21 22:43:28 -0600

    something little

--- module1.scm
+++ module1.scm
@@ -14,18 +14,19 @@
           ((equal? 1 n) (car L))
           ((devuelvePos (cdr L) (- n 1) )))))
 
-;;Dominio: arboles binarios de expresion y numeros x y
-;;Codominio: resultado de la expresion evaluada con x y
+;;Dominio: arboles binarios de expresion y numeros a b
+;;Codominio: expresion evaluada con a b
 
 (define evalua
   (lambda (L a b)
     (eval (list (cons 'lambda (cons '(x y) (list L))) a b))))
 
-;; Dominio: 
+;; Dominio: vacio
 ;; Codominio: x y o un numero natural entre 0 y 1000000
 (define x-y-#
   (lambda ()
     (cond ((= (random 3) 0) '(x))
           ((= (random 3) 1) '(y) )
           ((random 1000000)))))
+
 
